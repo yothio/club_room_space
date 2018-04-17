217d95ec86b78742e2da7688d021ccfbd88420e3
shiota yoshiki <shiotyan317@gmail.com>
2018-04-17 19:18:28 +0900

    first commit

---
+++ app/src/main/java/jp/prog_club/clubroom/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.prog_club.clubroom
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
